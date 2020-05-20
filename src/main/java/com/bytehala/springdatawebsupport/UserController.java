ff2632fe7cef0ac19d61f99ea77cc9efe8e27470
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-05-20 02:42:58 -0600

    PageableHandlerMethodArgumentResolver usages have been demo-ed so far

--- src/main/java/com/bytehala/springdatawebsupport/UserController.java
+++ src/main/java/com/bytehala/springdatawebsupport/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,8 +20,10 @@ public class UserController {
         return user;
     }
 
+    // Allows us to call /users?page=0&size=2
     @GetMapping("/users")
-    public Page<User> findAllUsers(@PageableDefault(value = 2, page = 0) Pageable pageable) {
+    public Page<User> findAllUsers(@RequestParam("page") int page,
+                                   @RequestParam("size") int size, Pageable pageable) {
         return userRepository.findAll(pageable);
     }
 }
