d0c6d819d123bd1c80601f3ead2297578f6d0dd9
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-05-20 02:35:48 -0600

    Add findAllUsers(Pageable)

--- src/main/java/com/bytehala/springdatawebsupport/UserController.java
+++ src/main/java/com/bytehala/springdatawebsupport/UserController.java
@@ -1,5 +1,8 @@
 package com.bytehala.springdatawebsupport;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/users/{id}")
     public User findUserById(@PathVariable("id") User user) {
         return user;
+    }
+
+    @GetMapping("/users")
+    public Page<User> findAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable);
     }
 }
