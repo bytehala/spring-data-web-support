1338975b2bf7958d889a007202acc0519c79501f
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-05-20 03:19:48 -0600

    @SortedDefault annotation

--- src/main/java/com/bytehala/springdatawebsupport/UserController.java
+++ src/main/java/com/bytehala/springdatawebsupport/UserController.java
@@ -3,7 +3,9 @@ package com.bytehala.springdatawebsupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +30,8 @@ public class UserController {
     }
 
     @GetMapping("/sortedusers")
-    public Page<User> findAllUsersSortedByName(@RequestParam("sort") String sort, Pageable pageable) {
+    public Page<User> findAllUsersSortedByName(@SortDefault(sort = "name",
+            direction = Sort.Direction.ASC) Pageable pageable) {
         return userRepository.findAll(pageable);
     }
 }
