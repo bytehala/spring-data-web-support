6cc8d0259fa2d6dc0d0409264aa412c80b8fc924
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-05-20 00:54:34 -0600

    Genesis

---
+++ src/main/java/com/bytehala/springdatawebsupport/SpringDataWebSupportApplication.java
@@ -0,0 +1,13 @@
+package com.bytehala.springdatawebsupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWebSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataWebSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytehala/springdatawebsupport/SpringDataWebSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytehala.springdatawebsupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataWebSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
