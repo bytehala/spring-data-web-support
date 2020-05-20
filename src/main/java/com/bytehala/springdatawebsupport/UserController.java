d9564bac566096bddf8f1e78581ff52de29f1ad3
Lem Dulfo <lemuel.dulfo@gmail.com>
2020-05-20 02:37:30 -0600

    How to use @PageableDefault annotation

--- src/main/java/com/bytehala/springdatawebsupport/UserController.java
+++ src/main/java/com/bytehala/springdatawebsupport/UserController.java
@@ -3,6 +3,7 @@ package com.bytehala.springdatawebsupport;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class UserController {
     }
 
     @GetMapping("/users")
-    public Page<User> findAllUsers(Pageable pageable) {
+    public Page<User> findAllUsers(@PageableDefault(value = 2, page = 0) Pageable pageable) {
         return userRepository.findAll(pageable);
     }
 }
